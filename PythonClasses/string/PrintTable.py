dfe8ddec9c8cf8b0b13c9e45d7b8df0cd1236ae6
SandeepPrajapati <prajapati87sandeep@gmail.com>
2023-10-06 17:48:17 +0530

    printtable_Task

--- PythonClasses/string/PrintTable.py
+++ PythonClasses/string/PrintTable.py
@@ -1,9 +1,14 @@
 # Take a input from a user and print the table
 
-tableOfNumber = int(input("Enter the number you want to print table :: "))
+number = int(input("Enter the number : \t"))
 
-# 2 x 1 = 2
-# 2 x 2 = 4
-
-print(f""{tableOfNumber} )
-#print(tableOfNumber + " x " + 1 + " = " + tableOfNumber*1)
+print(str(number) +"x" + "1" + "=" + str(number*1))
+print(str(number) +"x" + "2" + "=" + str(number*2))
+print(str(number) +"x" + "3" + "=" + str(number*3))
+print(str(number) +"x" + "4" + "=" + str(number*4))
+print(str(number) +"x" + "5" + "=" + str(number*5))
+print(str(number) +"x" + "6" + "=" + str(number*6))
+print(str(number) +"x" + "7" + "=" + str(number*7))
+print(str(number) +"x" + "8" + "=" + str(number*8))
+print(str(number) +"x" + "9" + "=" + str(number*9))
+print(str(number) +"x" + "10" + "=" + str(number*10))
