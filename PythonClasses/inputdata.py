05ab308767ff0119707f843af8f5f25e7b5a98f0
Sandeep Prajapati <sandeepprajapati@192.168.1.10>
2023-10-06 16:18:52 +0530

    .DS_Store banished!

---
+++ PythonClasses/inputdata.py
@@ -0,0 +1,4 @@
+# input("Enter User Name... ")
+
+firstName = input("Enter First Name... ")
+print("First Name : \t " + firstName)
---
+++ PythonClasses/string/PrintTable.py
@@ -0,0 +1,9 @@
+# Take a input from a user and print the table
+
+tableOfNumber = int(input("Enter the number you want to print table :: "))
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+
+print(f""{tableOfNumber} )
+#print(tableOfNumber + " x " + 1 + " = " + tableOfNumber*1)
